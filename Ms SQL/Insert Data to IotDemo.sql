ee4026cd83d37f36bc3385a28fd8e9b54a0e606e
AJeab <chalermchon_vis@hotmail.com>
2021-06-21 16:24:40 +0700

    Update Insert Data to IotDemo.sql

--- Ms SQL/Insert Data to IotDemo.sql
+++ Ms SQL/Insert Data to IotDemo.sql
@@ -1,17 +1,11 @@
-Use IotDB
-  
- insert into IotDemo (metric,value,machine)
-(
-SELECT
-'Temperature' AS [metric]
-,FLOOR(RAND()*(80-20+1))+20 AS [value]
-, 'm01' as [machine]
+VALUES
+     ('Temperature', FLOOR(RAND()*(25-15+1))+15 , 'm01') 
+    ,('Humidity' , FLOOR(RAND()*(90-40+1))+40 , 'm01') 
+    ,('Counter1' , FLOOR(RAND()*(50-30+1))+30 , 'm01') 
+    ,('Counter2' , FLOOR(RAND()*(50-30+1))+30 , 'm02') 
+    ,('Counter3' , FLOOR(RAND()*(50-30+1))+30 , 'm03') 
 
-UNION
+go
+insert into IotDemo (metric,value,machine)
+(SELECT 'Temperature' AS [metric] ,FLOOR(RAND()*(25-15+1))+15 AS [value] , 'm01' as [machine])
 
-SELECT
-'Humidity'  AS [metric]
-,FLOOR(RAND()*(80-20+1))+20 AS [value]
-, 'm01' as [machine]
-
-)  
