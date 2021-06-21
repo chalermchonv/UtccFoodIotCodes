87ee19935999809c810ba9e50fbd8b46f8eaf32b
AJeab <chalermchon_vis@hotmail.com>
2021-06-21 17:12:55 +0700

    Update Insert Data to IotDemo.sql

--- Ms SQL/Insert Data to IotDemo.sql
+++ Ms SQL/Insert Data to IotDemo.sql
@@ -1,15 +1,22 @@
-use IotDB
+
+ use IotDB
 
 insert into IotDemo (metric,value,machine)
-VALUES
-     ('Temperature', FLOOR(RAND()*(25-15+1))+15 , 'm01') 
-    ,('Humidity' , FLOOR(RAND()*(90-40+1))+40 , 'm01') 
-    ,('Counter1' , FLOOR(RAND()*(50-30+1))+30 , 'm01') 
-    ,('Counter2' , FLOOR(RAND()*(50-30+1))+30 , 'm02') 
-    ,('Counter3' , FLOOR(RAND()*(50-30+1))+30 , 'm03') 
+VALUES ('Temperature', FLOOR(RAND()*(25-15+1))+15 , 'm01') 
 
-go
 insert into IotDemo (metric,value,machine)
-(SELECT 'Temperature' AS [metric] ,FLOOR(RAND()*(25-15+1))+15 AS [value] , 'm01' as [machine])
+VALUES     ('Humidity' , FLOOR(RAND()*(90-40+1))+40 , 'm01') 
+
+insert into IotDemo (metric,value,machine)
+VALUES  ('Counter1' , FLOOR(RAND()*(50-30+1))+30 , 'm01') 
+
+insert into IotDemo (metric,value,machine)
+VALUES  ('Counter2' , FLOOR(RAND()*(50-30+1))+30 , 'm02') 
+    
+insert into IotDemo (metric,value,machine)
+VALUES  ('Counter3' , FLOOR(RAND()*(50-30+1))+30 , 'm03') 
+
+insert into IotDemo (metric,value,machine)
+(SELECT 'Counter1' AS [metric] ,FLOOR(RAND()*(50-30+1))+30 AS [value] , 'm01' as [machine])
 
 
